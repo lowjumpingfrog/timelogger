11e9ab6bd34b6f86bbe15f91510f0c5d4f6cb631
JENorton <lowjumpingfrog@gmail.com>
2017-12-01 14:35:26 -0700

    settings change

--- timelogger/settings/production.py
+++ timelogger/settings/production.py
@@ -92,6 +92,9 @@ WSGI_APPLICATION = 'timelogger.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+    'default': {
+            'ENGINE': 'django.db.backends.mysql',
+    }
 
 }
 import dj_database_url
