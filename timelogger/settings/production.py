4776ae67ee0edb7227378172474516227190a125
JENorton <lowjumpingfrog@gmail.com>
2017-12-01 13:31:58 -0700

    Updated Settings

--- timelogger/settings/production.py
+++ timelogger/settings/production.py
@@ -115,6 +115,17 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Certs
+CORS_REPLACE_HTTPS_REFERER      = True
+HOST_SCHEME                     = "https://"
+SECURE_PROXY_SSL_HEADER         = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT             = True
+SESSION_COOKIE_SECURE           = True
+CSRF_COOKIE_SECURE              = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
+SECURE_HSTS_SECONDS             = 1000000
+SECURE_FRAME_DENY               = True
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
